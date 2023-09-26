0ab7153bb7bdfbc749967bfbd1e606e46cd1a3b8
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-26 20:12:39 +0300

    Added comments

--- CircularQueueBasedOnArray/queue_ds.c
+++ CircularQueueBasedOnArray/queue_ds.c
@@ -1,29 +1,68 @@
-#ifndef _QUEUE_DS_H_
-#define _QUEUE_DS_H_
+/**
+  * @file queue_ds.c
+  * @brief This file contains the implementation of the Queue Data Structure
+  * @date 26/9/2023 20:12
+  * @version 1.0
+  */
 
-#include "..\std_types.h"
+#include "queue_ds.h"
 
-#define QUEUE_DEBUG
-
-typedef struct
+  /**
+	* @brief This function checks if the queue is full
+	* @param[in] queue: pointer to the queue object
+	* @return queue_status_t: status of the operation
+	*/
+static queue_status_t queue_full(queue_t* queue)
 {
-	void** queue_Array;
-	sint32_t queue_Front;
-	sint32_t queue_Rear;
-	uint32_t queue_Max_Size;
-	uint32_t queue_Element_Count;
-}queue_t;
+	queue_status_t ret = QUEUE_FULL; /* NULL pointer passed to the function */
 
-typedef enum
+	if (NULL == queue)
+	{
+		ret = QUEUE_NULL_POINTER;
+	}
+	else
+	{
+		if (queue->queue_Element_Count == (queue->queue_Max_Size))
+		{
+			ret = QUEUE_FULL;
+		}
+		else
+		{
+			ret = QUEUE_NOT_FULL;
+		}
+	}
+
+	return ret;
+}
+
+/**
+  * @brief This function checks if the queue is empty
+  * @param[in] queue: pointer to the queue object
+  * @return queue_status_t: status of the operation
+  */
+static queue_status_t queue_empty(queue_t* queue)
 {
-	QUEUE_OK,
-	QUEUE_NOK,
-	QUEUE_FULL,
-	QUEUE_NOT_FULL,
-	QUEUE_EMPTY,
-	QUEUE_NOT_EMPTY,
-	QUEUE_NULL_POINTER,
-}queue_status_t;
+	queue_status_t ret = QUEUE_NOK;
+
+	if (NULL == queue)
+	{
+		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+	}
+	else
+	{
+		/* Validate if the queue is not empty */
+		if (0 == queue->queue_Element_Count)
+		{
+			ret = QUEUE_EMPTY; /* Queue is empty */
+		}
+		else
+		{
+			ret = QUEUE_NOT_EMPTY; /* Queue is not empty */
+		}
+	}
+
+	return ret;
+}
 
 /**
   * @brief This function creates a queue object
@@ -31,39 +70,212 @@ typedef enum
   * @param[in] queue_Max_Size: maximum number of elements in the queue
   * @return queue_t*: pointer to the queue object
   */
-queue_t* queue_init(queue_status_t* ret_status, uint32_t queue_Max_Size);
+queue_t* queue_init(queue_status_t* ret_status, uint32_t queue_Max_Size)
+{
+	queue_t* my_Queue;
+	/* Create queue object in the heap to hold the queue information */
+	my_Queue = (queue_t*)malloc(sizeof(queue_t));
+	if (NULL == ret_status)
+	{
+		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+		my_Queue = NULL;
+	}
+	else
+	{
+		if (NULL == my_Queue) {
+			*ret_status = QUEUE_NOK; /* Memory allocation failed */
+			my_Queue = NULL;
+		}
+		else {
+			/* Create array of <void *> to hold the addresses of the Queue elements */
+			my_Queue->queue_Array = (void**)calloc(queue_Max_Size, sizeof(void*));
+			if (NULL == my_Queue->queue_Array)
+			{
+				*ret_status = QUEUE_NOK; /* Memory allocation failed */
+				my_Queue = NULL;
+			}
+			else
+			{
+				my_Queue->queue_Element_Count = 0;
+				my_Queue->queue_Front = -1;
+				my_Queue->queue_Rear = -1;
+				(my_Queue->queue_Max_Size) = queue_Max_Size;
+				*ret_status = QUEUE_OK; /* Queue created successfully */
+			}
+		}
+	}
+
+	return my_Queue;
+}
 
 /**
-  * @brief This function enqueues an element in the queue
+  * @brief This function adds an element to the queue
   * @param[in] queue: pointer to the queue object
-  * @param[in] data: pointer to the data to be enqueued
+  * @param[in] data: pointer to the data to be added to the queue
   * @return queue_status_t: status of the operation
   */
-queue_status_t queue_enqueue(queue_t* queue, void* data);
+queue_status_t queue_enqueue(queue_t* queue, void* data)
+{
+	queue_status_t ret = QUEUE_NOK;
+
+	if (NULL == queue)
+	{
+		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+	}
+	else
+	{
+		/* Validate if the queue is not full */
+		if (QUEUE_FULL == queue_full(queue))
+		{
+			ret = QUEUE_FULL; /* Queue is full, can't enqueue new element */
+		}
+		else
+		{
+			/* Increment Queue Rear Index */
+			(queue->queue_Rear)++;
+			/* Queue wraps to element 0 (Circular Queue) */
+			if (queue->queue_Rear == queue->queue_Max_Size)
+			{
+				queue->queue_Rear = 0;
+			}
+			else { /*Nothing */ }
+			/* Insert the input data in to the rear position */
+			queue->queue_Array[queue->queue_Rear] = data;
+			/* validate if the queue was empty, (front and rear points to the first element) */
+			if (QUEUE_EMPTY == queue_empty(queue)) {
+				queue->queue_Front = 0;
+				queue->queue_Element_Count = 1;
+			}
+			else {
+				(queue->queue_Element_Count)++; /* New element has been added to the Queue */
+			}
+			ret = QUEUE_OK; /* Element has been added to the Queue */
+		}
+	}
+
+	return ret;
+}
 
 /**
-  * @brief This function dequeues an element from the queue
+  * @brief This function removes an element from the queue
   * @param[in] queue: pointer to the queue object
   * @param[in] ret_status: pointer to the status of the operation
-  * @return void*: pointer to the dequeued data
+  * @return void*: pointer to the data removed from the queue
   */
-void* queue_dequeue(queue_t* queue, queue_status_t* ret_status);
+void* queue_dequeue(queue_t* queue, queue_status_t* ret_status)
+{
+	void* ret_element = NULL;
+
+	if ((NULL == queue) || (NULL == ret_status))
+	{
+		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+		ret_element = NULL;
+	}
+	else
+	{
+		/* Checks if the queue is empty */
+		if (QUEUE_EMPTY == queue_empty(queue))
+		{
+			*ret_status = QUEUE_EMPTY; /* Queue is empty, can't dequeue element */
+			ret_element = NULL;
+		}
+		else
+		{
+			/* Queue is not empty */
+			ret_element = queue->queue_Array[queue->queue_Front];
+			/* Increment Queue Front Index */
+			(queue->queue_Front)++;
+			/* Queue front was wrapped to element 0 (Circular Queue) */
+			if (queue->queue_Front == queue->queue_Max_Size)
+			{
+				queue->queue_Front = 0;
+			}
+			else
+			{
+				/*Nothing */
+			}
+			/* Check if the element is the last element in the Queue */
+			if (1 == queue->queue_Element_Count) {
+				queue->queue_Front = -1; /* Queue is empty */
+				queue->queue_Rear = -1;  /* Queue is empty */
+			}
+			else
+			{
+				/* Nothing */
+			}
+			(queue->queue_Element_Count)--; /* Element has been deleted from the Queue */
+			*ret_status = QUEUE_OK;
+		}
+	}
+
+	return &ret_element;
+}
 
 /**
-  * @brief This function returns the front element of the queue
+  * @brief This function returns the front element in the queue
   * @param[in] queue: pointer to the queue object
   * @param[in] ret_status: pointer to the status of the operation
-  * @return void*: pointer to the front element of the queue
+  * @return void*: pointer to the front element in the queue
   */
-void* queue_front(queue_t* queue, queue_status_t* ret_status);
+void* queue_front(queue_t* queue, queue_status_t* ret_status)
+{
+	void* ret_element = NULL;
+	if ((NULL == queue) || (NULL == ret_status))
+	{
+		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+		ret_element = NULL;
+	}
+	else
+	{
+		/* Checks if the queue is empty */
+		if (QUEUE_EMPTY == queue_empty(queue))
+		{
+			*ret_status = QUEUE_EMPTY; /* Queue is empty */
+			ret_element = NULL;
+		}
+		else
+		{
+			/* Queue is not empty */
+			ret_element = queue->queue_Array[queue->queue_Front];
+			*ret_status = QUEUE_OK; /* Element returned successfully */
+		}
+	}
+
+	return &ret_element;
+}
 
 /**
-  * @brief This function returns the rear element of the queue
+  * @brief This function returns the rear element in the queue
   * @param[in] queue: pointer to the queue object
   * @param[in] ret_status: pointer to the status of the operation
-  * @return void*: pointer to the rear element of the queue
+  * @return void*: pointer to the rear element in the queue
   */
-void* queue_rear(queue_t* queue, queue_status_t* ret_status);
+void* queue_rear(queue_t* queue, queue_status_t* ret_status)
+{
+	void* ret_element = NULL;
+	if ((NULL == queue) || (NULL == ret_status))
+	{
+		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+		ret_element = NULL;
+	}
+	else
+	{
+		/* Checks if the queue is empty */
+		if (QUEUE_EMPTY == queue_empty(queue))
+		{
+			*ret_status = QUEUE_EMPTY; /* Queue is empty */
+			ret_element = NULL;
+		}
+		else
+		{
+			/* Queue is not empty */
+			ret_element = queue->queue_Array[queue->queue_Rear];
+			*ret_status = QUEUE_OK; /* Element returned successfully */
+		}
+	}
+
+	return &ret_element;
+}
 
 /**
   * @brief This function returns the number of elements in the queue
@@ -71,15 +283,44 @@ void* queue_rear(queue_t* queue, queue_status_t* ret_status);
   * @param[in] ret_count: pointer to the number of elements in the queue
   * @return queue_status_t: status of the operation
   */
-queue_status_t queue_count(queue_t* queue, uint32_t* ret_count);
+queue_status_t queue_count(queue_t* queue, uint32_t* ret_count)
+{
+	queue_status_t ret = QUEUE_NOK;
+
+	if ((NULL == queue) || (NULL == ret_count))
+	{
+		*ret_count = 0;
+		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+	}
+	else
+	{
+		*ret_count = queue->queue_Element_Count;
+		ret = QUEUE_OK; /* Element returned successfully */
+	}
+
+	return ret;
+}
 
 /**
-  * @brief This function checks if the queue is empty
+  * @brief This function destroys the queue object
   * @param[in] queue: pointer to the queue object
-  * @param[in] ret_status: pointer to the status of the operation
   * @return queue_status_t: status of the operation
   */
-queue_status_t queue_destroy(queue_t* queue);
+queue_status_t queue_destroy(queue_t* queue)
+{
+	queue_status_t ret = QUEUE_NOK;
 
+	if (NULL == queue)
+	{
+		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
+	}
+	else
+	{
+		free(queue->queue_Array);
+		free(queue);
+		ret = QUEUE_OK; /* Queue destroyed successfully */
+	}
 
-#endif
+	return ret;
+}
+
--- CircularQueueBasedOnArray/queue_ds.h
+++ CircularQueueBasedOnArray/queue_ds.h
@@ -1,68 +1,29 @@
-/**
-  * @file queue_ds.c
-  * @brief This file contains the implementation of the Queue Data Structure
-  * @date 2/1/2020 1:00:00 AM
-  * @version 1.0
-  */
+#ifndef _QUEUE_DS_H_
+#define _QUEUE_DS_H_
 
-#include "queue_ds.h"
+#include "..\std_types.h"
 
-/**
-  * @brief This function checks if the queue is full
-  * @param[in] queue: pointer to the queue object
-  * @return queue_status_t: status of the operation
-  */
-static queue_status_t queue_full(queue_t* queue)
+#define QUEUE_DEBUG
+
+typedef struct
 {
-	queue_status_t ret = QUEUE_FULL; /* NULL pointer passed to the function */
+	void** queue_Array;
+	sint32_t queue_Front;
+	sint32_t queue_Rear;
+	uint32_t queue_Max_Size;
+	uint32_t queue_Element_Count;
+}queue_t;
 
-	if (NULL == queue)
-	{
-		ret = QUEUE_NULL_POINTER;
-	}
-	else
-	{
-		if (queue->queue_Element_Count == (queue->queue_Max_Size))
-		{
-			ret = QUEUE_FULL;
-		}
-		else
-		{
-			ret = QUEUE_NOT_FULL;
-		}
-	}
-
-	return ret;
-}
-
-/**
-  * @brief This function checks if the queue is empty
-  * @param[in] queue: pointer to the queue object
-  * @return queue_status_t: status of the operation
-  */
-static queue_status_t queue_empty(queue_t* queue)
+typedef enum
 {
-	queue_status_t ret = QUEUE_NOK; 
-
-	if (NULL == queue)
-	{
-		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-	}
-	else
-	{
-		/* Validate if the queue is not empty */
-		if (0 == queue->queue_Element_Count)
-		{
-			ret = QUEUE_EMPTY; /* Queue is empty */
-		}
-		else
-		{
-			ret = QUEUE_NOT_EMPTY; /* Queue is not empty */
-		}
-	}
-
-	return ret;
-}
+	QUEUE_OK,
+	QUEUE_NOK,
+	QUEUE_FULL,
+	QUEUE_NOT_FULL,
+	QUEUE_EMPTY,
+	QUEUE_NOT_EMPTY,
+	QUEUE_NULL_POINTER,
+}queue_status_t;
 
 /**
   * @brief This function creates a queue object
@@ -70,212 +31,39 @@ static queue_status_t queue_empty(queue_t* queue)
   * @param[in] queue_Max_Size: maximum number of elements in the queue
   * @return queue_t*: pointer to the queue object
   */
-queue_t* queue_init(queue_status_t* ret_status, uint32_t queue_Max_Size)
-{
-	queue_t* my_Queue;
-	/* Create queue object in the heap to hold the queue information */
-	my_Queue = (queue_t*)malloc(sizeof(queue_t));
-	if (NULL == ret_status)
-	{
-		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-		my_Queue = NULL;
-	}
-	else
-	{
-		if (NULL == my_Queue) {
-			*ret_status = QUEUE_NOK; /* Memory allocation failed */
-			my_Queue = NULL;
-		}
-		else {
-			/* Create array of <void *> to hold the addresses of the Queue elements */
-			my_Queue->queue_Array = (void**)calloc(queue_Max_Size, sizeof(void*));
-			if (NULL == my_Queue->queue_Array)
-			{
-				*ret_status = QUEUE_NOK; /* Memory allocation failed */
-				my_Queue = NULL;
-			}
-			else
-			{
-				my_Queue->queue_Element_Count = 0;
-				my_Queue->queue_Front = -1;
-				my_Queue->queue_Rear = -1;
-				(my_Queue->queue_Max_Size) = queue_Max_Size;
-				*ret_status = QUEUE_OK; /* Queue created successfully */
-			}
-		}
-	}
-
-	return my_Queue;
-}
+queue_t* queue_init(queue_status_t* ret_status, uint32_t queue_Max_Size);
 
 /**
-  * @brief This function adds an element to the queue
+  * @brief This function enqueues an element in the queue
   * @param[in] queue: pointer to the queue object
-  * @param[in] data: pointer to the data to be added to the queue
+  * @param[in] data: pointer to the data to be enqueued
   * @return queue_status_t: status of the operation
   */
-queue_status_t queue_enqueue(queue_t* queue, void* data)
-{
-	queue_status_t ret = QUEUE_NOK;
-
-	if (NULL == queue)
-	{
-		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-	}
-	else
-	{
-		/* Validate if the queue is not full */
-		if (QUEUE_FULL == queue_full(queue))
-		{
-			ret = QUEUE_FULL; /* Queue is full, can't enqueue new element */
-		}
-		else
-		{
-			/* Increment Queue Rear Index */
-			(queue->queue_Rear)++;
-			/* Queue wraps to element 0 (Circular Queue) */
-			if (queue->queue_Rear == queue->queue_Max_Size)
-			{
-				queue->queue_Rear = 0;
-			}
-			else { /*Nothing */ }
-			/* Insert the input data in to the rear position */
-			queue->queue_Array[queue->queue_Rear] = data;
-			/* validate if the queue was empty, (front and rear points to the first element) */
-			if (QUEUE_EMPTY == queue_empty(queue)) {
-				queue->queue_Front = 0;
-				queue->queue_Element_Count = 1;
-			}
-			else {
-				(queue->queue_Element_Count)++; /* New element has been added to the Queue */
-			}
-			ret = QUEUE_OK; /* Element has been added to the Queue */
-		}
-	}
-
-	return ret;
-}
+queue_status_t queue_enqueue(queue_t* queue, void* data);
 
 /**
-  * @brief This function removes an element from the queue
+  * @brief This function dequeues an element from the queue
   * @param[in] queue: pointer to the queue object
   * @param[in] ret_status: pointer to the status of the operation
-  * @return void*: pointer to the data removed from the queue
+  * @return void*: pointer to the dequeued data
   */
-void* queue_dequeue(queue_t* queue, queue_status_t* ret_status)
-{
-	void* ret_element = NULL;
-
-	if ((NULL == queue) || (NULL == ret_status))
-	{
-		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-		ret_element = NULL;
-	}
-	else
-	{
-		/* Checks if the queue is empty */
-		if (QUEUE_EMPTY == queue_empty(queue))
-		{
-			*ret_status = QUEUE_EMPTY; /* Queue is empty, can't dequeue element */
-			ret_element = NULL; 
-		}
-		else
-		{
-			/* Queue is not empty */
-			ret_element = queue->queue_Array[queue->queue_Front];
-			/* Increment Queue Front Index */
-			(queue->queue_Front)++;
-			/* Queue front was wrapped to element 0 (Circular Queue) */
-			if (queue->queue_Front == queue->queue_Max_Size)
-			{
-				queue->queue_Front = 0;
-			}
-			else
-			{
-				/*Nothing */
-			}
-			/* Check if the element is the last element in the Queue */
-			if (1 == queue->queue_Element_Count) {
-				queue->queue_Front = -1; /* Queue is empty */
-				queue->queue_Rear = -1;  /* Queue is empty */
-			}
-			else
-			{
-				/* Nothing */
-			}
-			(queue->queue_Element_Count)--; /* Element has been deleted from the Queue */
-			*ret_status = QUEUE_OK; 
-		}
-	}
-
-	return &ret_element;
-}
+void* queue_dequeue(queue_t* queue, queue_status_t* ret_status);
 
 /**
-  * @brief This function returns the front element in the queue
+  * @brief This function returns the front element of the queue
   * @param[in] queue: pointer to the queue object
   * @param[in] ret_status: pointer to the status of the operation
-  * @return void*: pointer to the front element in the queue
+  * @return void*: pointer to the front element of the queue
   */
-void* queue_front(queue_t* queue, queue_status_t* ret_status)
-{
-	void* ret_element = NULL;
-	if ((NULL == queue) || (NULL == ret_status))
-	{
-		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-		ret_element = NULL;
-	}
-	else
-	{
-		/* Checks if the queue is empty */
-		if (QUEUE_EMPTY == queue_empty(queue))
-		{
-			*ret_status = QUEUE_EMPTY; /* Queue is empty */
-			ret_element = NULL;
-		}
-		else
-		{
-			/* Queue is not empty */
-			ret_element = queue->queue_Array[queue->queue_Front]; 
-			*ret_status = QUEUE_OK; /* Element returned successfully */
-		}
-	}
-
-	return &ret_element;
-}
+void* queue_front(queue_t* queue, queue_status_t* ret_status);
 
 /**
-  * @brief This function returns the rear element in the queue
+  * @brief This function returns the rear element of the queue
   * @param[in] queue: pointer to the queue object
   * @param[in] ret_status: pointer to the status of the operation
-  * @return void*: pointer to the rear element in the queue
+  * @return void*: pointer to the rear element of the queue
   */
-void* queue_rear(queue_t* queue, queue_status_t* ret_status)
-{
-	void* ret_element = NULL;
-	if ((NULL == queue) || (NULL == ret_status))
-	{
-		*ret_status = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-		ret_element = NULL;
-	}
-	else
-	{
-		/* Checks if the queue is empty */
-		if (QUEUE_EMPTY == queue_empty(queue))
-		{
-			*ret_status = QUEUE_EMPTY; /* Queue is empty */
-			ret_element = NULL;
-		}
-		else
-		{
-			/* Queue is not empty */
-			ret_element = queue->queue_Array[queue->queue_Rear];
-			*ret_status = QUEUE_OK; /* Element returned successfully */
-		}
-	}
-
-	return &ret_element;
-}
+void* queue_rear(queue_t* queue, queue_status_t* ret_status);
 
 /**
   * @brief This function returns the number of elements in the queue
@@ -283,44 +71,15 @@ void* queue_rear(queue_t* queue, queue_status_t* ret_status)
   * @param[in] ret_count: pointer to the number of elements in the queue
   * @return queue_status_t: status of the operation
   */
-queue_status_t queue_count(queue_t* queue, uint32_t* ret_count)
-{
-	queue_status_t ret = QUEUE_NOK; 
-	 
-	if ((NULL == queue) || (NULL == ret_count))
-	{
-		*ret_count = 0;
-		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-	}
-	else
-	{
-		*ret_count = queue->queue_Element_Count;
-		ret = QUEUE_OK; /* Element returned successfully */
-	}
-
-	return ret;
-}
+queue_status_t queue_count(queue_t* queue, uint32_t* ret_count);
 
 /**
-  * @brief This function destroys the queue object
+  * @brief This function checks if the queue is empty
   * @param[in] queue: pointer to the queue object
+  * @param[in] ret_status: pointer to the status of the operation
   * @return queue_status_t: status of the operation
   */
-queue_status_t queue_destroy(queue_t* queue)
-{
-	queue_status_t ret = QUEUE_NOK;
+queue_status_t queue_destroy(queue_t* queue);
 
-	if (NULL == queue)
-	{
-		ret = QUEUE_NULL_POINTER; /* NULL pointer passed to the function */
-	}
-	else
-	{
-		free(queue->queue_Array);
-		free(queue);
-		ret = QUEUE_OK; /* Queue destroyed successfully */
-	}
 
-	return ret;
-}
-
+#endif

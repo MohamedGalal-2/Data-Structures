b7644a08cbde63480bebdc923217600f1acfaa47
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-10-03 10:30:41 +0300

    renamed file

---
+++ Static_Stack/std_types.h
@@ -0,0 +1,18 @@
+#ifndef _STD_TYPES_H_
+#define _STD_TYPES_H_
+
+#include <stdio.h>
+
+#define ZERO_INIT 0
+#define ZERO      0
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
+typedef char sint8_t;
+typedef short int sint16_t;
+typedef int sint32_t;
+typedef long long int sint64_t;
+
+#endif // _STD_TYPES_H_
